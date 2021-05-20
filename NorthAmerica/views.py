a4027813c8f39c00b9514aab19febbe804d7cfa6
pschassberger <pschassberger@gmail.com>
2021-05-20 10:46:19 -0600

    working with plot

--- NorthAmerica/views.py
+++ NorthAmerica/views.py
@@ -13,27 +13,8 @@ https://plotly.com/python/bubble-maps/#bubble-map-with-goscattergeo'''
 
 # Create your views here.
 def NorthAmericaView(request):
-    '''df = px.data.gapminder().query("year==2007")
-    fig = px.scatter_geo(df, locations="iso_alpha", color="continent",
-                        hover_name="country", size="pop",
-                        projection="natural earth")'''
-
-
-    '''fig = go.Figure(go.Scattergeo())
-    fig.update_geos(
-        resolution=50,
-        showcoastlines=True, coastlinecolor="RebeccaPurple", scope="north america",
-        showland=True, landcolor="LightGreen",
-        showocean=True, oceancolor="LightBlue",
-        showlakes=True, lakecolor="Blue",
-        showrivers=True, rivercolor="Blue"
-    )
-    fig.update_layout(height=300, margin={"r":0,"t":0,"l":0,"b":0})
-    map_plot = plot({'data': fig}, output_type='div')'''
-
     # add two dataframes
     us_cities = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/us-cities-top-1k.csv")
-    print(us_cities.head())
     platforms = gpd.read_file("files/platform.zip")
 
     temp = pd.DataFrame()
@@ -60,20 +41,3 @@ def NorthAmericaView(request):
     map_plot = plot({'data': fig}, output_type='div')
 
     return render(request, 'northAmericanMap.html', context={'map_plot': map_plot})
-
-
-us_cities = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/us-cities-top-1k.csv")
-#print(us_cities.head())
-platforms = gpd.read_file("files/platform.zip")
-
-temp = pd.DataFrame()
-
-temp['lon'] = platforms['geometry'].x
-temp['lat'] = platforms['geometry'].y
-
-platforms['lat'] = temp['lat']
-platforms['lon'] = temp['lon']
-
-data_dict = {"cities": us_cities, "platforms": platforms}
-
-print(data_dict['platforms']['lon'])
